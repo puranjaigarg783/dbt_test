9ae6c303bc3c8a8ac6674ed9a011bd0122ebb1b4
Puranjai Garg <puranjaigarg783@gmail.com>
2021-09-23 17:02:35 +0530

    new commit

--- dbt_test/models/example/test_dbt_model.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized='table') }}
-
-with test as (
-    select * from "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF100TCL"."CUSTOMER" limit 100
-    )
-select C_BIRTH_YEAR,COUNT(C_CUSTOMER_ID) from test group by C_BIRTH_YEAR
--- dbt_test/models/example/test_dbt_model_two.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized='table') }}
-
-with try as (
-    select * from "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF100TCL"."WEB_SALES" limit 10000
-    )
-select WS_SOLD_DATE_SK, sum(WS_NET_PROFIT) from try group by WS_SOLD_DATE_SK
